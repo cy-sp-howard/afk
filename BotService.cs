563fd37691654eb5c1068f146be69284b1e39815
cy-sp-howard <cy.sp.howard@gmail.com>
2025-12-24 17:32:36 +0800

    add channeling duration and trigger btn 1 when kepp alive started

--- BotService.cs
+++ BotService.cs
@@ -31,37 +31,40 @@ namespace BhModule.Afk
         }
         public void Init()
         {
-            KeepAliveTimer = new Timer(module.Settings.KeepAliveInterval.Value * 1000);
+            var s = module.Settings;
+            KeepAliveTimer = new Timer(s.KeepAliveInterval.Value * 1000);
             KeepAliveTimer.Elapsed += async delegate {
                 bool isInCombat = GameService.Gw2Mumble.PlayerCharacter.IsInCombat;
                 if (isInCombat)
                 {
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.HealButton.Value.PrimaryKey);
-                    if (module.Settings.HealButton.Value.PrimaryKey != 0) await Task.Delay(1000);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton2.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton2.Value.PrimaryKey != 0) await Task.Delay(200);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton3.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton3.Value.PrimaryKey != 0) await Task.Delay(200);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton4.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton4.Value.PrimaryKey != 0) await Task.Delay(200);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton5.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton5.Value.PrimaryKey != 0) await Task.Delay(200);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton6.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton6.Value.PrimaryKey != 0) await Task.Delay(200);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton7.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton7.Value.PrimaryKey != 0) await Task.Delay(200);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton8.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton8.Value.PrimaryKey != 0) await Task.Delay(200);
-                    Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton9.Value.PrimaryKey);
-                    if (module.Settings.KeepAliveButton9.Value.PrimaryKey != 0) await Task.Delay(200);
+                    Keyboard.Stroke((VirtualKeyShort)s.HealButton.Value.PrimaryKey);
+                    if (s.HealButton.Value.PrimaryKey != 0) await Task.Delay((int)(s.HealButtonWaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton2.Value.PrimaryKey);
+                    if (s.KeepAliveButton2.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton2WaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton3.Value.PrimaryKey);
+                    if (s.KeepAliveButton3.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton3WaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton4.Value.PrimaryKey);
+                    if (s.KeepAliveButton4.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton4WaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton5.Value.PrimaryKey);
+                    if (s.KeepAliveButton5.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton5WaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton6.Value.PrimaryKey);
+                    if (s.KeepAliveButton6.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton6WaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton7.Value.PrimaryKey);
+                    if (s.KeepAliveButton7.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton7WaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton8.Value.PrimaryKey);
+                    if (s.KeepAliveButton8.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton8WaitTime.Value * 1000));
+                    Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton9.Value.PrimaryKey);
+                    if (s.KeepAliveButton9.Value.PrimaryKey != 0) await Task.Delay((int)(s.KeepAliveButton9WaitTime.Value * 1000));
                 }
-                Keyboard.Stroke((VirtualKeyShort)module.Settings.KeepAliveButton1.Value.PrimaryKey);
+                Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton1.Value.PrimaryKey);
+                Task.Delay((int)(s.KeepAliveButton1WaitTime.Value * 1000)).GetAwaiter().GetResult();
 
 
             };
-            if (module.Settings.KeepAlive.Value)
+            if (s.KeepAlive.Value)
             {
                 KeepAliveTimer.Start();
+                Keyboard.Stroke((VirtualKeyShort)s.KeepAliveButton1.Value.PrimaryKey);
             }
         }
     }
--- ModuleSettings.cs
+++ ModuleSettings.cs
@@ -1,5 +1,6 @@
 ﻿using Blish_HUD;
 using Blish_HUD.Controls;
+using Blish_HUD.Controls.Extern;
 using Blish_HUD.Graphics.UI;
 using Blish_HUD.Input;
 using Blish_HUD.Settings;
@@ -27,6 +28,16 @@ namespace BhModule.Afk
         public SettingEntry<KeyBinding> KeepAliveButton8 { get; private set; }
         public SettingEntry<KeyBinding> KeepAliveButton9 { get; private set; }
         public SettingEntry<KeyBinding> HealButton { get; private set; }
+        public SettingEntry<float> KeepAliveButton1WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton2WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton3WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton4WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton5WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton6WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton7WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton8WaitTime { get; private set; }
+        public SettingEntry<float> KeepAliveButton9WaitTime { get; private set; }
+        public SettingEntry<float> HealButtonWaitTime { get; private set; }
         public ModuleSettings(AfkModule module, SettingCollection settings)
         {
             this.module = module;
@@ -51,15 +62,29 @@ namespace BhModule.Afk
             this.KeepAliveButton7 = settings.DefineSetting(nameof(this.KeepAliveButton7), new KeyBinding(Keys.None), () => "Trigger Button 7", () => "");
             this.KeepAliveButton8 = settings.DefineSetting(nameof(this.KeepAliveButton8), new KeyBinding(Keys.None), () => "Trigger Button 8", () => "");
             this.KeepAliveButton9 = settings.DefineSetting(nameof(this.KeepAliveButton9), new KeyBinding(Keys.None), () => "Trigger Button 9", () => "");
+            this.KeepAliveButton1WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton1WaitTime), 0.5f, () => "Skill 1 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton2WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton2WaitTime), 0.5f, () => "Skill 2 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton3WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton3WaitTime), 0.5f, () => "Skill 3 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton4WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton4WaitTime), 0.5f, () => "Skill 4 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton5WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton5WaitTime), 0.5f, () => "Skill 5 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton6WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton6WaitTime), 0.5f, () => "Skill 6 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton7WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton7WaitTime), 0.5f, () => "Skill 7 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton8WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton8WaitTime), 0.5f, () => "Skill 8 Channeling Duration (sec)", () => "");
+            this.KeepAliveButton9WaitTime = settings.DefineSetting(nameof(this.KeepAliveButton9WaitTime), 0.5f, () => "Skill 9 Channeling Duration (sec)", () => "");
             this.KeepAliveToggleKey.Value.Enabled = true;
             this.KeepAliveToggleKey.Value.Activated += ToggleKeepAlive;
 
             this.HealButton = settings.DefineSetting(nameof(this.HealButton), new KeyBinding(Keys.F), () => "Heal Button", () => "");
+            this.HealButtonWaitTime = settings.DefineSetting(nameof(this.HealButtonWaitTime), 0.5f, () => "Heal Skill Channeling Duration (sec)", () => "");
         }
         private void ToggleKeepAlive(object sender, System.EventArgs args)
         {
             KeepAlive.Value = !KeepAlive.Value;
-            if (KeepAlive.Value) module.BotService.KeepAliveTimer.Start();
+            if (KeepAlive.Value)
+            {
+                module.BotService.KeepAliveTimer.Start();
+                Blish_HUD.Controls.Intern.Keyboard.Stroke((VirtualKeyShort)KeepAliveButton1.Value.PrimaryKey);
+            }
             else module.BotService.KeepAliveTimer.Stop();
             Utils.Notify.Show(KeepAlive.Value ? "Enable Keep Alive." : "Disable Keep Alive.");
         }
@@ -78,7 +103,7 @@ namespace BhModule.Afk
             rootflowPanel = new FlowPanel()
             {
                 Size = buildPanel.Size,
-                FlowDirection = ControlFlowDirection.SingleTopToBottom,
+                FlowDirection = ControlFlowDirection.LeftToRight,
                 ControlPadding = new Vector2(5, 2),
                 OuterControlPadding = new Vector2(10, 15),
                 WidthSizingMode = SizingMode.Fill,
@@ -88,7 +113,7 @@ namespace BhModule.Afk
             };
             messagePadding = new Padding() { Parent = rootflowPanel };
 
-            foreach (var setting in settings.Where(s => s.SessionDefined))
+            foreach (var setting in settings.Where(s => s.SessionDefined && !s.EntryKey.Contains("WaitTime")))
             {
                 IView settingView;
 
@@ -107,8 +132,85 @@ namespace BhModule.Afk
                             settingViewInt.DisplayName = settingInt.GetDisplayNameFunc();
                         };
                     }
-                    if (!(settingView is SettingsView)) container.Show(settingView);
-                    if (setting.EntryKey == "KeepAliveButton1") new Label() { Parent = rootflowPanel, Text = "Trigger In Combat", AutoSizeWidth = true };
+                    if (!(settingView is SettingsView))
+                    {
+                        container.Show(settingView);
+                        if (setting.EntryKey.Contains("Button"))
+                        {
+                            var s = AfkModule.Instance.Settings;
+                            SettingEntry<float> ButtonWaitingSettingEntry = null;
+                            switch (setting.EntryKey)
+                            {
+                                case "KeepAliveButton1":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton1WaitTime;
+                                    break;
+                                case "KeepAliveButton2":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton2WaitTime;
+                                    break;
+                                case "KeepAliveButton3":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton3WaitTime;
+                                    break;
+                                case "KeepAliveButton4":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton4WaitTime;
+                                    break;
+                                case "KeepAliveButton5":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton5WaitTime;
+                                    break;
+                                case "KeepAliveButton6":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton6WaitTime;
+                                    break;
+                                case "KeepAliveButton7":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton7WaitTime;
+                                    break;
+                                case "KeepAliveButton8":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton8WaitTime;
+                                    break;
+                                case "KeepAliveButton9":
+                                    ButtonWaitingSettingEntry = s.KeepAliveButton9WaitTime;
+                                    break;
+                                case "HealButton":
+                                    ButtonWaitingSettingEntry = s.HealButtonWaitTime;
+                                    break;
+
+                            }
+                            if (ButtonWaitingSettingEntry is not null)
+                            {
+                                var t = new TextBox()
+                                {
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    Width = 100,
+                                    Height = 15,
+                                    Parent = rootflowPanel,
+                                    Text = ButtonWaitingSettingEntry.Value.ToString(),
+                                    BasicTooltipText = ButtonWaitingSettingEntry.GetDisplayNameFunc(),
+                                };
+                                t.InputFocusChanged += (sender, e) =>
+                                {
+                                    t.SelectionStart = 0;
+                                    t.SelectionEnd = e.Value ? t.Text.Length : 0;
+                                    if (!e.Value)
+                                    {
+                                        if (float.TryParse(t.Text, out float result) && result < 5 && result >= 0)
+                                        {
+                                            ButtonWaitingSettingEntry.Value = result;
+                                        }
+                                        else
+                                        {
+                                            t.Text = ButtonWaitingSettingEntry.Value.ToString();
+                                        }
+                                    }
+                                };
+                            }
+                        }
+                        else
+                        {
+                            container.Resized += (sender, e) =>
+                            {
+                                container.Width = rootflowPanel.ContentRegion.Width;
+                            };
+                        }
+                    }
+                    if (setting.EntryKey == "KeepAliveButton1") new Label() { Parent = rootflowPanel, Text = "Trigger In Combat", Width = rootflowPanel.ContentRegion.Width };
                 }
             }
 
